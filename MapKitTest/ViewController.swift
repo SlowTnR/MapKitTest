bfe8e52d3c49ce227462cdd123523ca0f6de2b2c
Ilja Patrushev <slowtnr@gmail.com>
2020-11-02 18:32:18 +0200

    MapKit user location.

--- MapKitTest/ViewController.swift
+++ MapKitTest/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let manager: CLLocationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    let mapDelta: Double = 0.1
+
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Ready!"
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else { return }
+        
+        label.text = "\(first.coordinate.longitude) | \(first.coordinate.latitude)"
+        
+        manager.stopUpdatingLocation()
+        
+        render(first)
+    }
+    
+    func render (_ location: CLLocation){
+        let coordinate = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: mapDelta, longitudeDelta: mapDelta)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
+    
+    
 }
 
