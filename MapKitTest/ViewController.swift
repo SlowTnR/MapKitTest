9c815a47d0697e4744f8b45caa2844bc268fedf6
Ilja Patrushev <slowtnr@gmail.com>
2020-11-04 11:46:07 +0200

    Add new branch. Leha, privetiki :* User location done.

--- MapKitTest/ViewController.swift
+++ MapKitTest/ViewController.swift
@@ -12,16 +12,15 @@ import MapKit
 
 class ViewController: UIViewController {
     
-    let manager: CLLocationManager = CLLocationManager()
+   
     @IBOutlet weak var mapView: MKMapView!
-    
-    
-    let mapDelta: Double = 0.1
-    let regionInMetters: Double = 10000
-
-
-    
     @IBOutlet weak var label: UILabel!
+    
+    let locationManager:CLLocationManager = CLLocationManager()
+    let regionInMetters: Double = 10000
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -30,39 +29,61 @@ class ViewController: UIViewController {
         checkLocationServices()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
-
-    
-    func render (_ location: CLLocation){
-        let coordinate = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: mapDelta, longitudeDelta: mapDelta)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        mapView.setRegion(region, animated: true)
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        mapView.addAnnotation(pin)
+    func centerViewUserLocation(){
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMetters, longitudinalMeters: regionInMetters)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert
+        }
+    }
     
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewUserLocation()
+            locationManager.startUpdatingLocation()
+            
+            break
+        case .denied:
+            // show alert hot to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // show alert with error
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
 }
+
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let first = locations.first else { return }
-        
-        label.text = "\(first.coordinate.longitude) | \(first.coordinate.latitude)"
-        
-        manager.stopUpdatingLocation()
-        
-        render(first)
+        guard let location = locations.last else {return}
+        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMetters, longitudinalMeters: regionInMetters)
+        mapView.setRegion(region, animated: true)
+        label.text = "\(location.coordinate.latitude) | \(location.coordinate.longitude)"
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
     }
 }
 
